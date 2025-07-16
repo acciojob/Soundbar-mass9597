84442ad7dd8246ca56ddc51178169df2a611a440
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-16 08:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let currentAudio = null;
+
+function playSound(fileName){
+	if(currentAudio){
+		currentAudio.pause();
+		currentAudio.currentAudio = 0;
+	}
+
+	currentAudio = new Audio('sound/${fileName}');
+	currentAudio.play();
+}
+
+function stopSound(){
+	if(currentAudio){
+		currentAudio.pause();
+		currentAudio.currentTime=0;
+	}
+}
